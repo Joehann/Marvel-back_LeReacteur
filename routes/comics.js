31af4fbdca2b74c72a35a09ae28ae6e69e1c3ffc
Johann Pouponnot <johannpouponnot@mbp-de-johann.lan>
2022-02-16 13:15:59 +0100

    feat: add route: /comics/:characterId

--- routes/comics.js
+++ routes/comics.js
@@ -16,4 +16,21 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+/*
+Route : /comics/:characterId | GET
+*/
+router.get("/comics/:characterId", async (req, res) => {
+  try {
+    //    console.log('route: /comics/:characterId')
+    const characterId = req.params.characterId;
+    const comics = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    res.json(comics.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;
